787232f3a5ee222b37af48b8dbd84286249feb67
Alison Caulfield <43919276+arcaulfield@users.noreply.github.com>
2019-11-10 11:09:39 -0500

    implemented saving of results for thresholding (#5)

--- src/thresholding.py
+++ src/thresholding.py
@@ -1,21 +1,31 @@
 from src.data_processing import clean_mordred_data
 from src.multi_output_regressor import run_multi_output_regressor
 from sklearn.feature_selection import VarianceThreshold
+from src.config import result_path
+import numpy as np
+import pandas as pd
+import os
 
 
 def find_threshold():
     X, y = clean_mordred_data()
 
-    run_multi_output_regressor(X, y)
-    n = 0.01
+    x_output = run_multi_output_regressor(X, y)
+    y_output = np.array([X.shape[1]])
 
-    for i in range(1000):
+    n = 0.172
+
+    for i in range(2000):
         print("Iteration " + str(i))
         sel = VarianceThreshold(threshold=n)
         train_x = sel.fit_transform(X)
         print("Removed descriptors with variance less than " + str(n) + ". X has size: " + str(train_x.shape[1]))
-        run_multi_output_regressor(train_x, y)
+        x_output = np.vstack((x_output, run_multi_output_regressor(train_x, y)))
+        y_output = np.vstack((y_output, train_x.shape[1]))
         n = n + 0.001
+
+    pd.DataFrame(x_output).to_csv(os.path.join(result_path, "x_output.csv"))
+    pd.DataFrame(y_output).to_csv(os.path.join(result_path, "y_output.csv"))
 
 
 if __name__ == '__main__':
