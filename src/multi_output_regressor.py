64f37b89127deda5eacf2fe548dfb82a5c2c57ba
Alison Caulfield <43919276+arcaulfield@users.noreply.github.com>
2019-11-09 19:33:59 -0500

    implemented variance thresholding (#3)

--- src/multi_output_regressor.py
+++ src/multi_output_regressor.py
@@ -5,9 +5,8 @@ from sklearn.linear_model import LinearRegression
 from src.data_processing import clean_mordred_data
 
 
-def run():
-    X, y = clean_mordred_data()
-    total_acc = np.zeros(shape=(X.shape[0]))
+def run_multi_output_regressor(X, y):
+    total_acc = np.zeros(shape=(y.shape[1]))
 
     kf = KFold(n_splits=5)
     for i, (train_index, valid_index) in enumerate(kf.split(X)):
@@ -18,17 +17,21 @@ def run():
         lr = LinearRegression()
         mor = MultiOutputRegressor(lr)
         mor.fit(x_train, y_train)
-        y_pred = mor.predict(x_valid)
+        y_pred = np.rint(mor.predict(x_valid))
 
         acc = accuracy_score(y_valid, y_pred)
-        print(f"Iteration {i+1}: Accuracy = {acc}%")
+        print(f"Iteration {i+1}: L2 = {acc}")
         total_acc = total_acc + acc
 
     print(f"Average accuracy = {total_acc/kf.get_n_splits()}")
+    return total_acc
 
 
 def accuracy_score(y_valid, y_pred):
-    dist = np.linalg.norm(y_valid - y_pred, axis=1)
+    dist = np.linalg.norm(y_valid - y_pred, ord=1, axis=0)
     return dist
 
 
+if __name__ == '__main__':
+    X, y = clean_mordred_data()
+    run_multi_output_regressor(X, y)
