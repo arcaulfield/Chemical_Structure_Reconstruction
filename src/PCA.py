9b64d64e242abc49673a4b5fbaad4dad24887c8c
Nadia Blostein <nadia.blostein@mail.mcgill.ca>
2019-11-11 10:35:12 -0500

    condensed PCA script

--- src/PCA.py
+++ src/PCA.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 get_ipython().magic('matplotlib inline')
@@ -12,18 +6,9 @@ from sklearn import preprocessing
 from sklearn.decomposition import PCA
 
 
-# In[2]:
-
-
 df = pd.read_csv('descriptors.csv')
 # dropping descriptor with strange values (does not actually affect PCA output)
 df=df.drop(['VR1_A'], axis=1)
-
-df.shape
-
-
-# In[3]:
-
 
 # Separating out & normalizing the features
 x = df.iloc[:, 1:1404].values
@@ -32,32 +17,15 @@ x = preprocessing.scale(x)
 # Separating out the target
 y = df.iloc[:,0].values
 
-
-# In[4]:
-
-
-# features = list(df.columns)
-# features.pop(0)
 targets = list(df['ID'])
-#targets
 
-
-# In[5]:
-
-
+# Running PCA
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
 principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
 
 
-# In[ ]:
-
-
 ##### TEST: How many components to select ##### 
-
-
-# In[185]:
-
 
 ### Testing 3 components ### 
     # pca = PCA(n_components=3)
@@ -66,20 +34,12 @@ principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
     # pca.explained_variance_ratio_
         # OUPUT: array([ 0.23354774,  0.11475039,  0.06810382])
 
-
-# In[186]:
-
-
 ### Testing 5 components ###
     # pca = PCA(n_components=5)
     # principalComponents = pca.fit_transform(x)
     # principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2','PC3','PC4','PC5'])
     # pca.explained_variance_ratio_
         # OUTPUT: array([ 0.23354774,  0.11475039,  0.06810382,  0.0412066 ,  0.03132345])
-
-
-# In[187]:
-
 
 ### Testing 7 components ### 
     # pca = PCA(n_components=7)
@@ -89,24 +49,12 @@ principalDf = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
         # OUTPUT: array([ 0.23354774,  0.11475039,  0.06810382,  0.04120661,  0.03132366,
             #0.02704538,  0.02453726])
 
-
-# In[ ]:
-
-
 ### Conclusion ### 
     # We selected the first two principal components (PCs) because the additional components
     # explain less than 7% of the variance 
     # (PC1: 23.4% of the variance; P2: 11.5% of the variance).
 
-
-# In[6]:
-
-
 finalDf = pd.concat([principalDf, df[['ID']]], axis = 1)
-
-
-# In[10]:
-
 
 ### PLOT: 2 Component PCA ###
         # x-axis = PC1, y-axis=PC2, data point = how much COMPOUND (NOT feature) is influenced by PCs 1 and 2
@@ -123,17 +71,9 @@ for target in targets:
                , s = 50)
 ax.grid()
 
-
-# In[7]:
-
-
 ### Variance explained by each component ###
 pca.explained_variance_ratio_
     # OUTPUT: array([ 0.23354774,  0.11475039])
-
-
-# In[17]:
-
 
 ### FUNCTION to generate heat map of each PC explained by descriptors ###
 
@@ -150,28 +90,19 @@ def heat_map(ar, n,m):
     plt.colorbar()
     plt.show()
 
-### Example: first 10 descriptors ###
-b = np.asarray(pca.components_[0,0:10])
-c = np.asarray(pca.components_[1,0:10])
-a = df.columns[1:11]
-d = [a,b,c]
-heat_map(d, 0, 1)
+    ### Example: first 10 descriptors ###
+    # b = np.asarray(pca.components_[0,0:10])
+    # c = np.asarray(pca.components_[1,0:10])
+    # a = df.columns[1:11]
+    # d = [a,b,c]
+    # heat_map(d, 0, 1)
 
-
-# In[19]:
-
-
-### How much each feature contributes to each component
+### How much each feature contributes to each component ### 
 pca.components_
 a = np.asarray(pca.components_)
-a.shape
 
-
-# In[20]:
-
-
-##### Important Feature Extraction #####
-    # Function: Thresholds & Displays the Data
+##### Function: Extraction of Important Features #####
+    # Thresholds & Displays the Data
     # c: array where rows = components, columns = descriptors, entries = contribution of descriptor to respective component
 def data_threshold(c,n, min, max):
     threshold = n
@@ -181,24 +112,14 @@ def data_threshold(c,n, min, max):
     e=c[d > n]
     e=np.asarray(e)
     t = e.shape[0]
-
-    # number of descriptors that respect the threshold
-    t= t/3
-    print(t)
-
+    t= t/3 # number of descriptors that respect the threshold
     f=(e[0:int(t)])
     g=(e[int(t):int(2*t)])
     h=(e[int(2*t):int(3*t)])
-
-        # creating array with only columns from c that respect threshold!
+    # creating array with only columns from c that respect threshold!
     final = np.vstack((f, g, h))
-    ###print(final.shape)
     pd.DataFrame(final).to_csv("descript_"+str(n)+".csv")
     heat_map(final,min,max)
-
-
-# In[22]:
-
 
 #### TEST: Thresholding & Displaying Data
 features = list(df.columns)
@@ -211,10 +132,4 @@ b.append(a[1])
 c = np.asarray(b)
 c.shape
 data_threshold(c, 0.08,0,1)
-
-
-# In[18]:
-
-
-print(c.shape)
 
